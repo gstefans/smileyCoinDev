eee1eb9e9d329ed93cba21b48dcc91bc60711316
Anton Vilhelm Ásgeirsson <anton.v.asgeirsson@gmail.com>
2021-05-18 10:34:56 +0000

    [ci] go back to depends for macos, bump darwin host versions

--- depends/hosts/darwin.mk
+++ depends/hosts/darwin.mk
@@ -1,6 +1,8 @@
-OSX_MIN_VERSION=10.10
-OSX_SDK_VERSION=10.14
-OSX_SDK=$(SDK_PATH)/MacOSX$(OSX_SDK_VERSION).sdk
+OSX_MIN_VERSION=10.12
+OSX_SDK_VERSION=10.15.1
+XCODE_VERSION=11.3.1
+XCODE_BUILD_ID=11C505
+OSX_SDK=$(SDK_PATH)/Xcode-$(XCODE_VERSION)-$(XCODE_BUILD_ID)-extracted-SDK-with-libcxx-headers
 darwin_CC=clang -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK)
 darwin_CXX=clang++ -target $(host) -mmacosx-version-min=$(OSX_MIN_VERSION) --sysroot $(OSX_SDK) -stdlib=libc++
 
