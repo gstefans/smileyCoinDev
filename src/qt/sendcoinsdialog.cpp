6ae0e433611a7d0222bc35fc207830a7acf77c52
Anton Vilhelm Ásgeirsson <antonva@users.noreply.github.com>
2021-06-28 12:38:23 +0000

    [smly] scripted-diff: change most mentions of Litecoin to Smileycoin
    Kept mentions of Litecoin changes in comments as they might be pertinent later.
    -BEGIN VERIFY SCRIPT-
    grep -rl Litecoin src/qt/* | xargs sed -i 's/Litecoin/Smileycoin/g'
    grep -rl litecoin src/qt/* | xargs sed -i 's/litecoin/smileycoin/g'
    -END VERIFY SCRIPT-

--- src/qt/sendcoinsdialog.cpp
+++ src/qt/sendcoinsdialog.cpp
@@ -807,7 +807,7 @@ void SendCoinsDialog::coinControlChangeEdited(const QString& text)
         }
         else if (!IsValidDestination(dest)) // Invalid address
         {
-            ui->labelCoinControlChangeLabel->setText(tr("Warning: Invalid Litecoin address"));
+            ui->labelCoinControlChangeLabel->setText(tr("Warning: Invalid Smileycoin address"));
         }
         else // Valid address
         {
